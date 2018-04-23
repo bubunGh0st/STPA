4df887618cf95ae27e1cc730065ad7017bdd9854
bubunGh0st <38558914+bubunGh0st@users.noreply.github.com>
2018-04-23 14:28:27 +1200

    edited sign up view

--- application/views/signUp.php
+++ application/views/signUp.php
@@ -16,36 +16,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <div class="form-row">
               <div class="col-md-6">
                 <label>First name</label>
-                <input class="form-control" type="text" aria-describedby="nameHelp" placeholder="Enter first name">
+                <input class="form-control" name="FName" type="text" aria-describedby="nameHelp" placeholder="Enter first name">
               </div>
               <div class="col-md-6">
                 <label>Last name</label>
-                <input class="form-control" type="text" aria-describedby="nameHelp" placeholder="Enter last name">
+                <input class="form-control" name="LName" type="text" aria-describedby="nameHelp" placeholder="Enter last name">
               </div>
             </div>
             <div class="form-row">
               <div class="col-md-6">
                 <label>Title</label>
-                <input class="form-control" type="text" aria-describedby="nameHelp" placeholder="Enter title">
+                <input class="form-control" name="Title" type="text" aria-describedby="nameHelp" placeholder="Enter title">
               </div>
               <div class="col-md-6">
                 <label>Email address</label>
-                <input class="form-control" type="email" aria-describedby="emailHelp" placeholder="Enter email">
+                <input class="form-control" name="Email" type="email" aria-describedby="emailHelp" placeholder="Enter email">
               </div>
             </div>
           </div>
           <div class="form-group">
             <label>Site <i data-toggle="modal" data-target="#modal-site-info" class="fa fa-question-circle text-danger" title="Please contact administrator if you do not find your site. Phone: 0123456789"></i></label>
             <div class="form-check checklist" >
-              <label class="form-check-label"><input class="form-check-input" type="checkbox"> WelTec Petone</label><br>
-              <label class="form-check-label"><input class="form-check-input" type="checkbox"> WelTec Wellington</label><br>
-              <label class="form-check-label"><input class="form-check-input" type="checkbox"> Whitirea Porirua</label><br>
-              <label class="form-check-label"><input class="form-check-input" type="checkbox"> Whitirea Paraparaumu</label><br>
-              <label class="form-check-label"><input class="form-check-input" type="checkbox"> Victoria University Kelburn</label><br>
-              <label class="form-check-label"><input class="form-check-input" type="checkbox"> Victoria University Vivian St</label>
+              <label class="form-check-label"><input class="form-check-input" name="SiteID[]" value="1"  type="checkbox"> WelTec Petone</label><br>
             </div>
           </div>
-          <a class="btn btn-primary btn-block" href="<?php echo(site_url());?>/signIn"><i class="fa fa-fw fa-user-plus"></i> Register</a>
+          <buton class="btn btn-primary btn-block" name="btnSubmit" href="<?php echo(site_url());?>/signIn"><i class="fa fa-fw fa-user-plus"></i> Register</button>
         </form>
         <div class="text-center">
           <a class="d-block small mt-3" href="<?php echo(site_url());?>/signIn">Back To Sign In Page</a>
